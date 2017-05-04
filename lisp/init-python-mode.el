87ee219012aa3f95d8922db24dd3061419fabc3c
saerpa <saerpa@163.com>
2017-05-04 22:23:41 +0800

    fix

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -1,25 +1,30 @@
+(require-package 'python-mode)
+(require-package 'ein)
+(require-package 'elpy)
+(require-package 'py-autopep8)
+
 (setq auto-mode-alist
       (append '(("SConstruct\\'" . python-mode)
  		("SConscript\\'" . python-mode))
               auto-mode-alist))
 
-(require-package 'ein)
-(require-package 'elpy)
-(require-package 'py-autopep8)
-
 (elpy-enable)
-(setq elpy-rpc-python-command "python3")  ;; python3
 ;;(elpy-use-ipython)                        ;; ipython
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+;; (setq python-shell-interpreter "ipython"
+;;       python-shell-interpreter-args "-i")
 
-;; (when (maybe-require-package 'anaconda-mode)
-;;   (after-load 'python
-;;     (add-hook 'python-mode-hook 'anaconda-mode)
-;;     (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
-;;   (when (maybe-require-package 'company-anaconda)
-;;     (after-load 'company
-;;       (add-hook 'python-mode-hook
-;;                 (lambda () (sanityinc/local-push-company-backend 'company-anaconda))))))
+(setq python-shell-interpreter "python3"
+      python-shell-interpreter-args "-i")
+
+(when (maybe-require-package 'anaconda-mode)
+  (after-load 'python
+    (add-hook 'python-mode-hook 'anaconda-mode)
+    (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
+  (when (maybe-require-package 'company-anaconda)
+    (after-load 'company
+      (add-hook 'python-mode-hook
+                (lambda () (sanityinc/local-push-company-backend 'company-anaconda))))))
 
 
 (provide 'init-python-mode)
