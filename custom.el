d1b706dd3ab6012033cc540a23f9326504b8be78
saerpa <saerpa@163.com>
2018-11-04 22:31:12 +0800

    Modify cursor color

--- custom.el
+++ custom.el
@@ -17,5 +17,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "文泉驿等宽微米黑" :foundry "WQYF" :slant normal :weight normal :height 113 :width normal)))))
- '(cursor ((t (:background "spring green"))))
+ '(default ((t (:family "文泉驿等宽微米黑" :foundry "WQYF" :slant normal :weight normal :height 113 :width normal))))
+ '(cursor ((t (:background "gold")))))
