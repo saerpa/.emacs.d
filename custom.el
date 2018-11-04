23c5003558af41075c139dae948f5dc4ed6cb5d1
saerpa <saerpa@163.com>
2018-11-04 21:35:47 +0800

    Little change

--- custom.el
+++ custom.el
@@ -14,4 +14,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Noto Sans Mono CJK SC" :foundry "GOOG" :slant normal :weight normal :height 120 :width normal)))))
+ '(default ((t (:family "文泉驿等宽微米黑" :foundry "WQYF" :slant normal :weight normal :height 113 :width normal)))))
