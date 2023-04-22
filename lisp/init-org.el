8b89c606cd82d3e8cc0098338bb27f8e0637381f
saerpa <saerpa@163.com>
2023-04-22 17:56:21 +0800

    Add org-roam settings

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -33,4 +33,44 @@
   (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
   (add-to-list 'org-structure-template-alist '("py" . "src python")))
 
+;; org-roam
+(use-package org-roam
+  :custom
+  (org-roam-directory "~/Org/RoamNotes/")
+  :bind
+  (("C-c n l" . org-roam-buffer-toggle)
+   ("C-c n f" . org-roam-node-find)
+   ("C-c n i" . org-roam-node-insert)
+   ("C-c n c" . org-roam-capture))
+  :config
+  (org-roam-setup)
+  (org-roam-db-autosync-mode))
+
+(setq org-roam-capture-templates
+      '(("m" "main" plain "%?"
+	 :if-new (file+head "main/${slug}.org"
+			    "#+title: ${title}\n")
+	 :immediate-finish t
+	 :unnarrowed t)
+	("r" "reference" plain "%?"
+	 :if-new (file+head "reference/${title}.org"
+			    "#+title: ${title}\n")
+	 :immediate-finish t
+	 :unnarrowed t)))
+
+;; Creating the property "type" on my nodes
+;; Ref: https://jethrokuan.github.io/org-roam-guide
+(cl-defmethod org-roam-node-type ((node org-roam-node))
+  "Return the TYPE of NODE."
+  (condition-case nil
+      (file-name-nondirectory
+       (directory-file-name
+	(file-name-directory
+	 (file-relative-name (org-roam-node-file node) org-roam-directory))))
+    (error "")))
+
+;; Modifying the display template to show the node "type"
+(setq org-roam-node-display-template
+      (concat "${type:15} ${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+
 (provide 'init-org)
